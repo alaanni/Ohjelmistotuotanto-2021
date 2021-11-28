5a3a723b97f917a99f6a21dc4ec9b9ad56284046
Anni Ala-Mattinen <anni.alamattinen@gmail.com>
2021-11-28 22:22:40 +0200

    testi hinta oikein yhden lisäyksen jälkeen

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -16,8 +16,10 @@ class Ostoskori:
         # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2 
 
     def hinta(self):
-        if len(self.kori) == 0:
-            return 0
+        count = 0
+        for t in self.kori:
+            count += t.hinta()
+        return count
         # kertoo korissa olevien ostosten yhteenlasketun hinnan
 
     def lisaa_tuote(self, lisattava: Tuote):
--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -16,3 +16,9 @@ class TestOstoskori(unittest.TestCase):
         maito = Tuote("Maito", 3)
         self.kori.lisaa_tuote(maito)
         self.assertEqual(self.kori.tavaroita_korissa(), 1)
+    
+    # step 3
+    def test_yhden_tuotteen_lisaamisen_jalkeen_hinta_oikein(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.assertEqual(self.kori.hinta(), 3)
