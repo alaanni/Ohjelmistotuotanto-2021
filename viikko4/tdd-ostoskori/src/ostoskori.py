5ae6d1d9d979e1685de5526dc88c28ad1e7027d1
Anni Ala-Mattinen <anni.alamattinen@gmail.com>
2021-11-28 21:48:35 +0200

    testi ostoskorin hinta ja tavaroiden määrä alussa ok

---
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -0,0 +1,35 @@
+from tuote import Tuote
+from ostos import Ostos
+
+class Ostoskori:
+    def __init__(self):
+        # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
+        self.kori = []
+
+    def tavaroita_korissa(self):
+        return len(self.kori)
+        # kertoo korissa olevien tavaroiden lukumäärän
+        # eli jos koriin lisätty 2 kpl tuotetta "maito", tulee metodin palauttaa 2 
+        # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2 
+
+    def hinta(self):
+        if len(self.kori) == 0:
+            return 0
+        # kertoo korissa olevien ostosten yhteenlasketun hinnan
+
+    def lisaa_tuote(self, lisattava: Tuote):
+        # lisää tuotteen
+        pass
+
+    def poista_tuote(self, poistettava: Tuote):
+        # poistaa tuotteen
+        pass
+
+    def tyhjenna(self):
+        pass
+        # tyhjentää ostoskorin
+
+    def ostokset(self):
+        pass
+        # palauttaa listan jossa on korissa olevat ostos-oliot
+        # kukin ostos-olio siis kertoo mistä tuotteesta on kyse JA kuinka monta kappaletta kyseistä tuotetta korissa on
---
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -0,0 +1,14 @@
+import unittest
+from ostoskori import Ostoskori
+from tuote import Tuote
+
+class TestOstoskori(unittest.TestCase):
+    def setUp(self):
+        self.kori = Ostoskori()
+
+    def test_ostoskorin_hinta_ja_tavaroiden_maara_alussa(self):
+        self.assertEqual(self.kori.hinta(), 0)
+        self.assertEqual(self.kori.tavaroita_korissa, 0)
+
+    def test_ostoskorin_hinta_ja_tavaroiden_maara_alussa(self):
+        self.assertEqual(self.kori.hinta(), 0)
