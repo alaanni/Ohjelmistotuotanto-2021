45e65eefd2bfebb64c1563aae9f83631e60dd12c
Anni Ala-Mattinen <anni.alamattinen@gmail.com>
2021-11-28 22:29:13 +0200

    testi hinta oikein kahden saman lisäyksen jälkeen

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -38,6 +38,12 @@ class TestOstoskori(unittest.TestCase):
         self.assertEqual(self.kori.hinta(), 8)
 
     # step 6
+    def test_kahden_saman_tuotteen_lisaamisen_jalkeen_korissa_kaksi(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
+        self.assertEqual(self.kori.tavaroita_korissa(), 2)
+        
     # step 7
     # step 8
     # step 9
